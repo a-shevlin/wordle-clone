22d5f41b02c93f7927f96b6d513796398b32fcc5
Alex Shevlin <alexshevlin1@gmail.com>
2022-10-16 01:18:12 -0700

    update App.js to include title and keyboard.

--- App.js
+++ App.js
@@ -1,20 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { colors } from "./src/constants";
+import Keyboard from "./src/components/Keyboard";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <StatusBar style="light" />
+      <Text style={styles.title}>WORDLE</Text>
+      <Keyboard/>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: colors.dark,
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    top: 40,
+    color: colors.light,
+    fontWeight: "bold",
+    fontSize: 32,
+    letterSpacing: 7,
+  }
 });
