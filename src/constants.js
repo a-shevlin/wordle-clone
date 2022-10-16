d0f4423e8df28203dc7dc3142b0d058f834cf88c
Alex Shevlin <alexshevlin1@gmail.com>
2022-10-16 01:17:26 -0700

    create base variables.

---
+++ src/constants.js
@@ -0,0 +1,24 @@
+export const colors = {
+  dark: "#2D3047",
+  light: "#edfaff",
+  grey: "#768787",
+  teal: "#588B8B",
+  pink: "#FFD5C2",
+  orange: "#F28F3B",
+  redorange: "#C8553D",
+};
+
+export const colorsToEmoji = {
+  [colors.dark]: "⬛",
+  [colors.teal]: "🟩",
+  [colors.redorange]: "🟧",
+};
+
+export const ENTER = "ENTER";
+export const CLEAR = "CLEAR";
+
+export const keys = [
+  ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p"],
+  ["a", "s", "d", "f", "g", "h", "j", "k", "l"],
+  [ENTER, "z", "x", "c", "v", "b", "n", "m", CLEAR],
+]
